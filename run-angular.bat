ec070008ffa0af2c1dd046e3ef9262034a41eeef
GeronimoCaporossiGomez <geronimogomezok123@gmail.com>
2025-05-28 10:06:02 -0300

    .

--- run-angular.bat
+++ run-angular.bat
@@ -1,7 +1,26 @@
 @echo off
-:: Forzar uso de Node local desde carpeta NodeJS
+:: -------------------------------
+:: SE PUEDE UTILIZAR HASTA EN TU CASA PERO ES PRINCIPALMENTE PARA LA ESCUELA
+:: Script de arranque de Statill
+:: Usa Node.js portable y Angular CLI local
+:: -------------------------------
+
+:: Rutas al ejecutable de Node y Angular CLI
 set "NODE_EXE=NodeJS\node.exe"
+set "NPM_CLI=NodeJS\npm.cmd"
+set "NG_CLI=node_modules\@angular\cli\bin\ng.js"
 
-:: Ejecutar Angular CLI desde node_modules local
-%NODE_EXE% node_modules/@angular/cli/bin/ng.js %*
+:: Mensaje inicial
+echo [STATILL] 🚀 Iniciando entorno portable...
 
+:: Verificar si Angular CLI está instalado (si falta node_modules o ng.js)
+IF NOT EXIST %NG_CLI% (
+    echo [STATILL] 📦 node_modules no encontrado. Instalando dependencias...
+    %NPM_CLI% install
+)
+
+:: Ejecutar Angular
+echo [STATILL] ✅ Ejecutando 'ng serve' con Node portable...
+%NODE_EXE% %NG_CLI% serve
+
+pause
