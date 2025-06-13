1e243c822e21da8ae1f9b379d929e7026714b707
MartinAdler3 <121838018+MartinAdler3@users.noreply.github.com>
2025-06-13 08:54:51 -0300

    push, header en la apartado de vendedor

--- src/app/comercio/ajustes/ajustes.component.ts
+++ src/app/comercio/ajustes/ajustes.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderStatillComponent } from 'src/app/header-statill/header-statill.component';
 import { ComercioHeaderComponent } from '../comercio-header/comercio-header.component';
 
 @Component({
   selector: 'app-ajustes',
   standalone: true,
-  imports: [CommonModule, ComercioHeaderComponent],
+  imports: [CommonModule, ComercioHeaderComponent, HeaderStatillComponent],
   templateUrl: './ajustes.component.html',
   styleUrl: './ajustes.component.scss'
 })
--- src/app/comercio/escanear/escanear.component.ts
+++ src/app/comercio/escanear/escanear.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ComercioHeaderComponent } from '../comercio-header/comercio-header.component';
 import { CommonModule } from '@angular/common';
+import { HeaderStatillComponent } from 'src/app/header-statill/header-statill.component';
 
 @Component({
   selector: 'app-escanear',
   standalone: true,
-  imports: [ComercioHeaderComponent, CommonModule],
+  imports: [ComercioHeaderComponent, CommonModule, HeaderStatillComponent],
   templateUrl: './escanear.component.html',
   styleUrl: './escanear.component.scss'
 })
