64e0479c16f6005f9f99e504402795b12019efdb
GeronimoCaporossiGomez <170636117+GeronimoCaporossiGomez@users.noreply.github.com>
2025-06-26 09:44:24 -0300

    Stock Actualización

--- src/app/comercio/stock/stock.component.ts
+++ src/app/comercio/stock/stock.component.ts
@@ -1,21 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms'; // Cambiado a forms modulee
 
 @Component({
   selector: 'app-stock',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule], // Usar FormsModule para el ngmodel
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss']
 })
 export class StockComponent {
-  SePuedeVerElformulario = false
+  SePuedeVerElformulario = false; // arranca escondidiwis
+  product = { // Objeto para almacenar datos del formulario
+    name: '',
+    code: '',
+    price: null,
+    description: '',
+    cantidad: null,
+  };
+
+  //invertimos
   FormChange() {
-this.SePuedeVerElformulario = !this.SePuedeVerElformulario
+    this.SePuedeVerElformulario = !this.SePuedeVerElformulario;
   }
-  GuardarData(FormData: any) {
-    console.log("Formulario enviado: ", FormData);
-    this.SePuedeVerElformulario = false
+
+  // Manejar envío del formulario
+  GuardarData() {
+    console.log("Formulario enviado: ", this.product);
+    // Resetear el objeto y ocultar formulario
+    this.product = {
+      name: '',
+      code: '',
+      price: null,
+      description: '',
+      cantidad: null,
+    };
+    this.SePuedeVerElformulario = false;
   }
 }
