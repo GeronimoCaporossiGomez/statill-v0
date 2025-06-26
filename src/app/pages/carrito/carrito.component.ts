37bf4c132b695313e98c837fffef04b30e7a8d58
MartinAdler3 <martinurieladler1@gmail.com>
2025-06-26 10:38:24 -0300

    apenas cambie algo

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -1,11 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carrito',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.scss'
 })
 export class CarritoComponent {
-
+numeroArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
