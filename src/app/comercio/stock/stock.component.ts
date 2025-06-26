65de77a1676c04b32c2577de3f8e3e6e81d2c0a4
GeronimoCaporossiGomez <170636117+GeronimoCaporossiGomez@users.noreply.github.com>
2025-06-26 10:48:17 -0300

    .

--- src/app/comercio/stock/stock.component.ts
+++ src/app/comercio/stock/stock.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-stock',
   standalone: true,
-  imports: [CommonModule, FormsModule], // Usar FormsModule para el ngmodel
+  imports: [CommonModule, FormsModule, SidebarComponent, RouterOutlet], // Usar FormsModule para el ngmodel
   templateUrl: './stock.component.html',
   styleUrls: ['./stock.component.scss']
 })
