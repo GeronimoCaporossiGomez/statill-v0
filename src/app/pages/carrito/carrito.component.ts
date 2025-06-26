b594c50248b542cbfa43a6a9d50445e546aafce2
GeronimoCaporossiGomez <170636117+GeronimoCaporossiGomez@users.noreply.github.com>
2025-06-26 19:05:35 -0300

    .

--- src/app/pages/carrito/carrito.component.ts
+++ src/app/pages/carrito/carrito.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   selector: 'app-carrito',
   imports: [CommonModule],
   templateUrl: './carrito.component.html',
-  styleUrl: './carrito.component.scss'
+  styleUrls: ['./carrito.component.scss']
 })
 export class CarritoComponent {
 numeroArray: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
