3c8670651bad963d3c2899665e66156d1bffd4d0
MartinAdler3 <martinurieladler1@gmail.com>
2025-10-09 17:18:57 -0300

    medio que intente hacer algo pero medio  que no funciono. Perdon, la fuente de toda esta mierda https://github.com/bluehalo/ngx-leaflet. Probablemente mas facil buscar otro coso

--- src/app/mapa/mapa.component.ts
+++ src/app/mapa/mapa.component.ts
@@ -1,20 +1,79 @@
-import { Component, AfterViewInit } from '@angular/core';
-import * as L from 'leaflet';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { latLng, tileLayer, MapOptions, Marker, marker, icon, Map, LeafletMouseEvent } from 'leaflet';
+import { MiApiService } from '../servicios/mi-api.service';
+import { LeafletDirective, LeafletModule } from '@bluehalo/ngx-leaflet';
 
 @Component({
   selector: 'app-mapa',
-  imports: [],
+  standalone: true,
+  imports: [LeafletModule],
   templateUrl: './mapa.component.html',
   styleUrl: './mapa.component.scss'
 })
-export class MapaComponent implements AfterViewInit {
-  private map!: L.Map;
+export class MapaComponent {
+  @Input() modo: 'vendedor' | 'comprador' = 'comprador';
+  @Input() ubicacion: [number, number] | null = null;
+  @Output() ubicacionSeleccionada = new EventEmitter<[number, number]>();
 
-  ngAfterViewInit(): void {
-    this.map = L.map('map').setView([40.4168, -3.7038], 13); // Coordenadas de ejemplo (Madrid)
+  options: MapOptions = {
+    layers: [
+      tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; OpenStreetMap contributors'
+      })
+    ],
+    zoom: 13,
+    center: latLng(-34.6037, -58.3816)
+  };
 
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; OpenStreetMap contributors'
-    }).addTo(this.map);
+  map!: Map;
+  leafletMarkers: Marker[] = [];
+
+  constructor(private api: MiApiService) {}
+
+  onMapReady(map: Map) {
+    this.map = map;
+    if (this.modo === 'vendedor') {
+      this.map.on('click', (e: LeafletMouseEvent) => {
+        const latlng: [number, number] = [e.latlng.lat, e.latlng.lng];
+        this.leafletMarkers = [
+          marker(latlng, {
+            icon: icon({
+              iconUrl: 'assets/img/mapa-pin-statill.png',
+              iconSize: [32, 32],
+              iconAnchor: [16, 32]
+            })
+          })
+        ];
+        this.ubicacionSeleccionada.emit(latlng);
+      });
+      if (this.ubicacion) {
+        this.leafletMarkers = [
+          marker(this.ubicacion, {
+            icon: icon({
+              iconUrl: 'assets/img/mapa-pin-statill.png',
+              iconSize: [32, 32],
+              iconAnchor: [16, 32]
+            })
+          })
+        ];
+        this.map.setView(this.ubicacion, 15);
+      }
+    } else {
+      this.api.getStores().subscribe((res: any) => {
+        if (res && res.data) {
+          this.leafletMarkers = res.data.filter((store: any) => store.lat && store.lng).map((store: any) =>
+            marker([store.lat, store.lng], {
+              icon: icon({
+                iconUrl: 'assets/img/mapa-pin-statill.png',
+                iconSize: [32, 32],
+                iconAnchor: [16, 32]
+              })
+            }).bindPopup(`<b>${store.name}</b><br>${store.address || ''}`)
+          );
+        }
+      });
+    }
   }
 }
+
--- src/app/pages/mapa-pagina/mapa-pagina.component.ts
+++ src/app/pages/mapa-pagina/mapa-pagina.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderStatillComponent } from 'src/app/Componentes/header-statill/header-statill.component';
+import { MapaComponent } from 'src/app/mapa/mapa.component';
 
 @Component({
   selector: 'app-mapa-pagina',
-  imports: [CommonModule, HeaderStatillComponent],
+  imports: [CommonModule, HeaderStatillComponent, MapaComponent],
   templateUrl: './mapa-pagina.component.html',
   styleUrl: './mapa-pagina.component.scss'
 })
