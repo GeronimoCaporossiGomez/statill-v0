c3d1cfaafd5e0a03b0bbaa9761f6fec4ecced3cf
manucorsu <manuelcorsunsky@gmail.com>
2025-11-13 16:39:23 -0300

    la home?

--- src/app/Componentes/Discounts-statill/Discounts-statill.component.ts
+++ src/app/Componentes/Discounts-statill/Discounts-statill.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Discount, MiApiService, Product, ProductsResponse, Store } from 'src/app/servicios/mi-api.service';
 
+
 @Component({
   selector: 'app-discounts-statill',
   standalone: true,
@@ -15,17 +16,23 @@ export class DiscountsStatillComponent {
   public store: Store;
   public img: string;
   private readonly api = inject(MiApiService);
-  ngOnInit(){
+  public text: string;
+  ngOnInit() {
     this.api.getProducto(this.d.product_id).subscribe({
-      next: (r)=>{
+      next: (r: any) => {
         this.product = r.data;
         this.api.getStoreById(this.product.store_id).subscribe({
-          next: (r)=>{
+          next: (r: any) => {
             this.store = r.data;
-            
+            this.api.getImageByObjectId("product", this.product .id).subscribe({
+              next: (r: any) => {
+                this.img = r.data;    this.text = `${this.d.pct_off}% OFF en ${this.product.name}`
+
+              }
+            });
           }
-        })
+        });
       },
-    })
+    });
   }
 }
